b53e30f4b2c020cb424ea2f0ce545759198972b6
jeelee <>
2025-04-07 16:41:13 +0900

    Refactor: add valid email at create account.

--- src/main/java/kr/co/jeelee/demoboard/domain/member/dto/request/MemberCreateRequest.java
+++ src/main/java/kr/co/jeelee/demoboard/domain/member/dto/request/MemberCreateRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 public record MemberCreateRequest(
         @NotBlank(message = "name can't be Blank.") String name,
         @NotBlank(message = "nickname can't be Blank.") String nickname,
-        String email,
+        @NotBlank(message = "email can't be Blank.") String email,
         @NotBlank(message = "password can't be Blank.") String password
 ) {}
